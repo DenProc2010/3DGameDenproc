4f30779d73d74944ea9058758984d121d4978ce8
DenProc2010 <146536374+DenProc2010@users.noreply.github.com>
2025-05-15 20:39:47 +0300

    Ineractable ob.

--- 3dGame/Assets/Scripts/InteractableObject.cs
+++ 3dGame/Assets/Scripts/InteractableObject.cs
@@ -1,11 +1,39 @@
 using UnityEngine;
 
-public class InteractableObject : MonoBehaviour 
+public class InteractableObject : MonoBehaviour
 {
     public string ItemName;
+
+    public bool playerInRange;
 
     public string GetItemName()
     {
         return ItemName;
-    }    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
--- 3dGame/Assets/Scripts/SelectionManager.cs
+++ 3dGame/Assets/Scripts/SelectionManager.cs
@@ -3,12 +3,26 @@ using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
 {
+
+    public static SelectionManager Instance { get; set; }
+
     public GameObject iteractionUI;
     Text interactionText;
+
+    public bool onTarget;
 
     private void Start()
     {
         interactionText = iteractionUI.GetComponent<Text>();
+    }
+
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }       
     }
 
     private void Update()
@@ -24,18 +38,21 @@ public class SelectionManager : MonoBehaviour
             var selectionTransform = hit.transform;
             var interactable = selectionTransform.GetComponent<InteractableObject>();
 
-            if (interactable != null)
+            if (interactable != null && interactable.playerInRange)
             {
+                onTarget = true;
                 interactionText.text = interactable.GetItemName();
                 iteractionUI.SetActive(true);
             }
             else
             {
+                onTarget = false;
                 iteractionUI.SetActive(false);
             }
         }
         else
         {
+            onTarget = false;
             iteractionUI.SetActive(false);
         }
     }
